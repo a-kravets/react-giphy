70025bebc4d0514f5981b2527c46a6ba4750d967
andriy kravets <4kravets@gmail.com>
2019-04-03 18:29:09 +0200

    adding gif_list, searchbar, giphy api

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,18 +1,52 @@
 import React, { Component } from 'react';
+import giphy from 'giphy-api';
+
 import SearchBar from './search_bar.jsx';
 import Gif from './gif.jsx';
+import GifList from './gif_list.jsx';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    const API_KEY = process.env.GIPHY_API;
+
+    this.state = {
+      gifs: [],
+      selectedGifId: "1dMPcTP8TdZ1mMO48C"
+    }
+    // this.search('pamela')
+  }
+
+  search = (query) => {
+    giphy('yj5WedHgKD3uSBYLkbUCn2j6N0AAChm2').search({
+      q: query,
+      rating: 'g',
+      limit: 10
+    }, (error, result) => {
+       this.setState({
+        gifs: result.data
+       });
+    });
+    }
+
   render() {
+    // const gifs = [
+    //   { id: "cJuJHFw9ZECRo9AbTR" },
+    //   { id: "2aRKm3ZfEFVjxHpafc" }
+    // ];
+
     return (
       <div>
         <div className="left-scene">
-          <SearchBar />
+          <SearchBar search={this.search}/>
           <div className="selected-gif">
-            <Gif id="1dMPcTP8TdZ1mMO48C"/>
+            <Gif id={this.state.selectedGifId} />
           </div>
         </div>
-        <div className="right-scene"></div>
+        <div className="right-scene">
+          <GifList gifs={this.state.gifs} />
+        </div>
     </div>
     );
   }
--- src/components/search_bar.jsx
+++ src/components/search_bar.jsx
@@ -1,9 +1,16 @@
 import React, { Component } from 'react';
+// import SearchBar from 'app.jsx'
 
 class SearchBar extends Component {
+  handleUpdate = (event) => {
+  // console.log(event.target.value);
+  this.props.search(event.target.value)
+  }
+
+
   render() {
     return (
-      <input type="text" className="form-search form-control"/>
+      <input type="text" className="form-search form-control" onChange={this.handleUpdate} />
     );
   }
 }

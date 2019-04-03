e7d32e338be4f89b040c0bd935da847b0e6e6e2f
andriy kravets <4kravets@gmail.com>
2019-04-03 22:47:09 +0200

    adding focus on search bar

--- src/components/search_bar.jsx
+++ src/components/search_bar.jsx
@@ -2,15 +2,27 @@ import React, { Component } from 'react';
 // import SearchBar from 'app.jsx'
 
 class SearchBar extends Component {
+    constructor(props) {
+      super(props);
+
+    this.state = {
+      focused: false
+    };
+  }
+
   handleUpdate = (event) => {
   // console.log(event.target.value);
   this.props.search(event.target.value)
   }
 
+  handleFocus = () => {
+  console.log('sas');
+  this.setState({focused: !this.state.focused})
+  }
 
   render() {
     return (
-      <input type="text" className="form-search form-control" onChange={this.handleUpdate} />
+      <input type="text" className={this.state.focused ? 'form-search form-control focused' : 'form-search form-control'} onChange={this.handleUpdate} onMouseEnter={this.handleFocus}/>
     );
   }
 }
